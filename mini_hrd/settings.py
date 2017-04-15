eaadc72ff6e9fd99db7077f4544f91281e1ae1ec
restureese <restuankanser@gmail.com>
2017-04-15 04:47:44 +0000

    menambahkan sb admin 2

--- mini_hrd/settings.py
+++ mini_hrd/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'assets')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'mini_hrd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 
 # agar dapat diakses berulangkali saat login_required view karyawan
 LOGIN_URL = '/login/'
+
+STATICFILES_DIRS = [STATIC_DIR,]
